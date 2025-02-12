494a3cb2a8dfa1e7010b0acb4982626caa461503
Sasidhar Sunkesula <sasidharsunkesula579@gmail.com>
2025-02-12 23:09:39 +0530

    added strapi and ws

---
+++ src/api/websocket/services/websocket.ts
@@ -0,0 +1,30 @@
+import { Server } from 'ws';
+
+let wss: Server;
+const port = 8080;
+export const startWebSocketServer = () => {
+    // Create WebSocket server
+    wss = new Server({ port: port }); // Replace with the desired port
+
+    wss.on('connection', (ws) => {
+        console.log('A new client has connected');
+
+        ws.on('message', (message) => {
+            console.log('Received message:', message);
+            // Echo back the message to the client
+            ws.send(`Echo: ${message}`);
+        });
+
+        ws.on('close', () => {
+            console.log('Client disconnected');
+        });
+    });
+};
+
+export const stopWebSocketServer = () => {
+    if (wss) {
+        wss.close(() => {
+            console.log('WebSocket server stopped');
+        });
+    }
+};
